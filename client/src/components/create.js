cdebfdcc26bfcc203c4f8be41febb48730605be0
Matt Murphy <mattmurph888@gmail.com>
2023-05-21 18:32:11 -0400

    starting to work on the text interpreter

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -8,10 +8,58 @@ export default function Create() {
 	const [time, setTime] = useState(0);
 
 	function handleTextChange(event) {
-		let curText = event.target.value
+		let curText = event.target.value;
+		let nestedArray = createNestedArray(curText);
 		setText(curText);
-		const lines = curText.split('\n');
-		console.log(lines);
+		console.log(nestedArray);
+	}
+
+	function createNestedArray(input) {
+		const lines = input.split('\n');
+		const nestedArray = [];
+
+		lines.forEach((line) => {
+			const tabsCount = countTabsAtStart(line);
+			console.log(`tab count: ${tabsCount}`);
+			const lineContent = line.trim(); // Remove leading/trailing spaces
+
+			let currentArray = nestedArray;
+			for (let i = 0; i < tabsCount; i++) {
+				if (!Array.isArray(currentArray[currentArray.length - 1])) {
+					currentArray[currentArray.length - 1] = [];
+				}
+				currentArray = currentArray[currentArray.length - 1];
+			}
+			currentArray.push(lineContent);
+		});
+
+		return nestedArray;
+	}
+
+	function countTabsAtStart(line) {
+		let tabCount = 0;
+		for (let i = 0; i < line.length; i++) {
+			if (line[i] === '\t') {
+				tabCount++;
+			} else {
+				break;
+			}
+		}
+		return tabCount;
+	}
+
+	function handleKeyDown(event) {
+		if (event.key === 'Tab') {
+			event.preventDefault();
+			const textarea = event.target;
+			const { selectionStart, selectionEnd } = textarea;
+			const value = textarea.value;
+			textarea.value =
+				value.substring(0, selectionStart) +
+				'\t' +
+				value.substring(selectionEnd);
+			textarea.selectionStart = textarea.selectionEnd = selectionStart + 1;
+		}
 	}
 
 	return (
@@ -22,6 +70,7 @@ export default function Create() {
 					value={text}
 					placeholder="Write your set here..."
 					onChange={handleTextChange}
+					onKeyDown={handleKeyDown}
 				/>
 				<div className="line-stats"></div>
 			</div>
